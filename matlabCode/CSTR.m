4a2671ceabc5c32a043af30b25d31cd22d8f6dc5
Jack <s1310295@sms.ed.ac.uk>
2017-10-06 16:02:21 +0100

    CSTR function
    A function the finds the steady state concentration inside a CSTR

---
+++ matlabCode/CSTR.m
@@ -0,0 +1,40 @@
+% solves the steady state outlets of an elementary reaction CSTR
+
+function [cSol] = CSTR(reactOrder, kVal, c0, volFlow, V)
+
+%check function variables (unfinished)
+kSize = size(kVal);
+rSize = size(reactOrder);
+cSize = size(c0);
+vSize = size(volFlow);
+
+if kSize(1) ~= rSize(1) || rSize(2) ~= cSize(2) || cSize(2) ~= vSize(2)
+    error('Function input dimensional mismatch')
+end
+
+%calc variables
+v0 = sum(volFlow);
+nReact = size(reactOrder);
+
+%solve steady state
+cSol = fsolve(@massBal,c0);
+
+    function [F] = massBal(c)
+        %evaluates the species balance
+        r = rateCalc(c);
+        F = volFlow.*c0/V+r-v0*c/V;
+    end
+
+    function [r] = rateCalc(c)
+        %evaluates the rate of reaction
+        c = repmat(c,nReact(1),1);
+        
+        elemOrder = reactOrder;
+        elemOrder(elemOrder>0)=0;
+        baseM = c.^(-elemOrder);
+        rReact = kVal.*prod(baseM,2);
+        r = rReact'*reactOrder;
+    end
+
+
+end
