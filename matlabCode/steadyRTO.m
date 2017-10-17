54cc851f011f244d4a70f53de6223f63885d0b10
Jack <s1310295@sms.ed.ac.uk>
2017-10-17 21:39:38 +0100

    tv edits

--- matlabCode/steadyRTO.m
+++ matlabCode/steadyRTO.m
@@ -2,25 +2,41 @@ function [] = steadyRTO(modelFun,modelGrad,plantFun,plantGrad,modelPara,plantPar
 % Takes the modelFun (either convex or other) and the plantFun and uses a
 % standard MA to work out the optimum position of the plant
 
+% Initial run
+if isempty(plantGrad)
+    l = dfunc(plantFun,u0,0);
+else
+    l = cellFuncHandle2vec(plantGrad,ones(size(plantGrad))*u(i-1),ones(size(plantGrad)));
+end
+
+p = cellFuncHandle2vec(plantFun,ones(size(plantFun))*u(i-1),ones(size(plantFun)));
+m = cellFuncHandle2vec(plantFun,ones(size(modelFun))*u(i-1),ones(size(modelFun)));
+
+
 first = 1;
 u = u0;
 i = 2;
 while solved == 0 || first == 1
     
     
-    %calc the current plant gradient
+    % calc the current plant gradient
     if isempty(plantGrad)
         pGrad = dfunc(plantFun,u0,0);
     else
-        pGrad = plantGrad(plantPara);
+        pGrad = cellFuncHandle2vec(plantGrad,ones(size(plantGrad))*u(i-1),ones(size(plantGrad)));
     end
     
-    %calc the current model gradient
+    % calc the current model gradient
     if isempty(modelGrad)
         mGrad = dfunc(modelFun,u0,0);
     else
-        mGrad = modelGrad(modelPara);
+        mGrad = cellFuncHandle2vec(modelGrad,ones(size(modelGrad))*u(i-1),ones(size(modelGrad)));
     end
+    
+    % calc the current value
+    p = cellFuncHandle2vec(plantFun,ones(size(plantFun))*u(i-1),ones(size(plantFun)));
+    m = cellFuncHandle2vec(plantFun,ones(size(modelFun))*u(i-1),ones(size(modelFun)));
+    
     
     
     i = i + 1;
