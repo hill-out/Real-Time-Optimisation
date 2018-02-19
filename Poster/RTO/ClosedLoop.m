b47f408f2aa51906e44a2774b2ec494d8ad47f8f
Jack <s1310295@sms.ed.ac.uk>
2018-02-19 12:06:29 +0000

    finished

--- Poster/RTO/ClosedLoop.m
+++ Poster/RTO/ClosedLoop.m
@@ -7,9 +7,10 @@ clear
 optionu = optimoptions('fmincon','Display','off');
 xGuess = [0.09, 0.36, 0.1, 0.25, 0.1, 0.1];
 
-[up_opt] = fmincon(@(x)phiFun(plantController(x)',CSTRplant(plantController(x)',xGuess)),...
-    [0.09, 12],[],[],[],[],[0,0],[1,50],...
-    @(x)conFun(plantController(x)',CSTRplant(plantController(x)',xGuess)),optionu);
+[rp_opt] = [0.12, 6.034];
+% fmincon(@(x)phiFun(plantController(x)',CSTRplant(plantController(x)',xGuess)),...
+%     [0.09, 12],[],[],[],[],[0,0],[1,50],...
+%     @(x)conFun(plantController(x)',CSTRplant(plantController(x)',xGuess)),optionu);
 
 
 % Run model 0
@@ -23,25 +24,26 @@ xGuess = [0.09, 0.36, 0.1, 0.25, 0.1, 0.1];
 [con0_opt(2), dcon0_opt(3:4)] = g2CR(r0_opt');
 
 % Run plant @u0_opt
-u0_opt = plantController(r0_opt);
+u0_opt = plantController(r0_opt)';
 [Xp] = CSTRplant(u0_opt, xGuess);
 
 % Get phi and g
 phip = phiFun(u0_opt,Xp);
 conp = conFun(u0_opt,Xp);
 
-du = diag([0.01, 0.01, 0.1]);
+dr = diag([0.0001, 0.01]);
 
-for i = 1:3
-    u = u0_opt + du(i,:);
-    dphip(i) = (phiFun(u, CSTRplant(u,Xp)) - phip)/du(i,i);
-    a = (conFun(u, CSTRplant(u,Xp)) - conp)/du(i,i);
+for i = 1:2
+    r = r0_opt + dr(i,:);
+    u = plantController(r)';
+    dphip(i) = (phiFun(u, CSTRplant(u,Xp)) - phip)/dr(i,i);
+    a = (conFun(u, CSTRplant(u,Xp)) - conp)/dr(i,i);
     dconp(i) = a(1);
-    dconp(i+3) = a(2);
+    dconp(i+2) = a(2);
 end
 
 % Get modifiers
-K =0.6;
+K =0.8;
 m0phi = K*(phip - phi0_opt);
 m0con = K*(conp - con0_opt);
 
@@ -49,39 +51,42 @@ m1phi = K*(dphip - dphi0_opt);
 m1con = K*(dconp - dcon0_opt);
 
 % Make modified model
-phiMod = @(u)(phiCU(u) + m0phi + m1phi*(u-u0_opt'));
-g1Mod = @(u)(g1CU(u) + m0con(1) + m1con(1:3)*(u-u0_opt'));
-g2Mod = @(u)(g2CU(u) + m0con(2) + m1con(4:6)*(u-u0_opt'));
+phiMod = @(r)(phiCR(r) + m0phi + m1phi*(r-r0_opt'));
+g1Mod = @(r)(g1CR(r) + m0con(1) + m1con(1:2)*(r-r0_opt'));
+g2Mod = @(r)(g2CR(r) + m0con(2) + m1con(3:4)*(r-r0_opt'));
 
 % set-up iterations
 unsolved = 1;
 k = 1;
-uGuess = u0_opt;
+rGuess = r0_opt;
 xGuess = Xp;
 
 while unsolved
     % Run model i
-    [ui_opt(k,:)] = fmincon(@(x)phiMod(x'),uGuess,[],[],[],[],...
-        [0,0,70],[20,50,120],@(x)deal([g1Mod(x'),g2Mod(x')],[]),optionu);
+    [ri_opt(k,:)] = fmincon(@(x)phiMod(x'),rGuess,[],[],[],[],...
+        [0,0],[1,50],@(x)deal([g1Mod(x'),g2Mod(x')],[]),optionu);
+    ui_opt(k,:) = plantController(ri_opt(k,:))';
     
     % Get phi and g
-    [phii_opt(k), dphii_opt(k,:)] =  phiCU(ui_opt(k,:)');
-    [coni_opt(k,1), dconi_opt(k,1:3)] = g1CU(ui_opt(k,:)');
-    [coni_opt(k,2), dconi_opt(k,4:6)] = g2CU(ui_opt(k,:)');
+    [phii_opt(k), dphii_opt(k,:)] =  phiCR(ri_opt(k,:)');
+    [coni_opt(k,1), dconi_opt(k,1:2)] = g1CR(ri_opt(k,:)');
+    [coni_opt(k,2), dconi_opt(k,3:4)] = g2CR(ri_opt(k,:)');
     
-    % Run plant @u0_opt
+    % Run plant @ui_opt
     [Xp(k,:)] = CSTRplant(ui_opt(k,:), xGuess);
     
     % Get phi and g
     phip(k) = phiFun(ui_opt(k,:),Xp(k,:));
     conp(k,:) = conFun(ui_opt(k,:),Xp(k,:));
     
-    for i = 1:3
-        u = ui_opt(k,:) + du(i,:);
-        dphip(k,i) = (phiFun(u, CSTRplant(u,Xp(k,:))) - phip(k))/du(i,i);
-        a = (conFun(u, CSTRplant(u,Xp(k,:))) - conp(k,:))/du(i,i);
+    for i = 1:2
+        r = ri_opt(k,:) + dr(i,:);
+        u = plantController(r)';
+        
+        dphip(k,i) = (phiFun(u, CSTRplant(u,Xp(k,:))) - phip(k))/dr(i,i);
+        a = (conFun(u, CSTRplant(u,Xp(k,:))) - conp(k,:))/dr(i,i);
         dconp(k,i) = a(1);
-        dconp(k,i+3) = a(2);
+        dconp(k,i+2) = a(2);
     end
     
     % Get modifiers
@@ -92,9 +97,9 @@ while unsolved
     m1con = (1-K)*m1con + K*(dconp(k,:) - dconi_opt(k,:));
     
     % Make modified model
-    phiMod = @(u)(phiCU(u) + m0phi + m1phi*(u-ui_opt(k,:)'));
-    g1Mod = @(u)(g1CU(u) + m0con(1) + m1con(1:3)*(u-ui_opt(k,:)'));
-    g2Mod = @(u)(g2CU(u) + m0con(2) + m1con(4:6)*(u-ui_opt(k,:)'));
+    phiMod = @(r)(phiCR(r) + m0phi + m1phi*(r-ri_opt(k,:)'));
+    g1Mod = @(r)(g1CR(r) + m0con(1) + m1con(1:2)*(r-ri_opt(k,:)'));
+    g2Mod = @(r)(g2CR(r) + m0con(2) + m1con(3:4)*(r-ri_opt(k,:)'));
     
     k = k + 1;
     if k > 10
@@ -102,4 +107,6 @@ while unsolved
     end
 end
 
-plot(ui_opt(:,1))
+plot([r0_opt(1);ri_opt(:,1)],[r0_opt(2);ri_opt(:,2)])
+hold on
+plot(rp_opt(1), rp_opt(2), 'rx')
